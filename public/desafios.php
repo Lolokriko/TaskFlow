4d12c62a3f11ea1b6675834f74785decb9959261
LoloKriko <loloricoinstituto@gmail.com>
2025-10-09 09:27:22 +0200

    2 ejercicio hecho solo (primos)

--- public/desafios.php
+++ public/desafios.php
@@ -1,17 +1,122 @@
 
+<br><br>
+
+    <!-- Desafíos de Lógica y Algoritmos Puros -->
+
+<?php
+
+    // Desafío 1: La Sucesión de Fibonacci
+
+
+    ?>
+
+<br><br>
+
+<?php
+
+    //Desafío 2: Comprobador de Números Primos
+
+    $numero = 29;
+    $esPrimo = null;
+
+    if ($numero <= 1) {
+        $esPrimo = false;
+    }
+
+        else {
+
+            for ($i = 2; $i <= sqrt($numero); $i++) {
+
+                if ($numero % $i == 0) {
+                    $esPrimo = false;
+                    break;
+
+                }
+            }
+        }
+
+            if ($esPrimo)
+    {
+        echo "El número $numero es primo<br>";
+    }
+
+    else
+
+    {
+        echo "El número $numero no es primo<br>";
+    }
+
+?>
+
+
+<br><br><br>
+
+    <!-- Desafíos de Manipulación de Datos -->
+
+<?php
+
+    // Desafío 3: ¿Es un Palíndromo?
+
+
+?>
+
+
+<br><br>
+
+
+<?php
+
+    // Desafío 4: Ordenación Manual de un Array (Bubble Sort)
+
+
+?>
+
+
+<br><br><br>
+
+    <!-- Mini-Proyecto: Simulador de Cajero Automático (ATM) -->
+
+
+<?php
+//
+
+
+?>
+
+<br><br>
+
+    <!-- Desafío Creativo: Arte con Bucles -->
+
     <?php
-    $pageTitle = "Problemas de PHP";
-    define("SITE_NAME", "TaskFlow");
-    $isPremiumUser = true;
+    //
+
+
     ?>
 
 
+
+
+
+
+
+
+
+<br><br><br><br>
+
+
+
+
+    <?php
+    define("SITE_NAME", "Desafios.php");
+    $nombre = "Lolo";
+    $edad = 19;
+    $isPremiumUser = false;
+    ?>
 
     <!DOCTYPE html>
     <html lang="es">
     <head>
         <meta charset="UTF-8">
-        <title><?php echo $pageTitle; ?></title>
     </head>
     <body>
         <header>
